02e7fec4a00fe8bde881ab3d4f1551d22839d05c
rh0st00 <ruh0st00@gmail.com>
2025-08-10 01:12:39 +0800

    Add files via upload

---
+++ character-data.js
@@ -0,0 +1,44 @@
+var characterSet = [
+	{
+		'index'	:0,
+		'name'	:'是我唷',
+		'san'	:95,
+		'serifList':[
+			{
+				'serif':'啊-啊',
+				'image':'R01'
+			}
+		],
+		'tunList':[
+			{
+				'serif':'原來如此？',
+				'image':'R01'
+			},
+			{
+				'serif':'嘻嘻，我喜歡這個',
+				'image':'R無外套笑'
+			},
+			{
+				'serif':'登登，骰子',
+				'image':'R骰子'
+			}
+		]
+	},
+	{
+		'index'	:1,
+		'name'	:'？？？',
+		'san'	:15,
+		'serifList':[
+			{
+				'serif':'......什麼？',
+				'image':'k01'
+			}
+		],
+		'tunList':[
+			{
+				'serif':'我、我不清楚...',
+				'image':'k01'
+			}
+		]
+	}
+];
